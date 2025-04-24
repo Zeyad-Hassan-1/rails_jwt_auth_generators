61eb7886e38b8860095d4ee99b235e8f0a0e2913
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-24 16:43:17 +0200

    v0.1.3

--- lib/generators/auth/jwt_generator.rb
+++ lib/generators/auth/jwt_generator.rb
@@ -1,20 +1,12 @@
 module Auth
-  class JwtGenerator < Rails::Generators::NamedBase
+  class JwtGenerator < Rails::Generators::Base  # Changed from NamedBase
+    desc "This generator creates JWT auth files (no arguments needed)"
     source_root File.expand_path('templates', __dir__)
 
-    desc "This generator creates a JWT auth concern and authentication controller"
-    
     def create_auth_files
-      # 1. Generate JWT Concern
       template "json_web_token.rb", "app/controllers/concerns/json_web_token.rb"
-
-      # 2. Generate Authentication Controller
       template "authentication_controller.rb", "app/controllers/authentication_controller.rb"
-
-      # 3. Generate UsersController
       template "users_controller.rb", "app/controllers/users_controller.rb"
-
-      # 4. Generate User Model (optional, but recommended)
       template "user.rb", "app/models/user.rb"
     end
   end
