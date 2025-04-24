adc78b7a7972cf4b7c8c68b438a2d2026484ddd9
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-24 14:49:52 +0200

    v1

---
+++ lib/rails_jwt_auth_generators.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_jwt_auth_generators/version"
+
+module RailsJwtAuthGenerators
+  class Error < StandardError; end
+  # Your code goes here...
+end
