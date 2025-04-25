85977767e77b070a5ffa6d4bf069527bfc13d421
Zeyad-Hassan-1 <studying.zezo@gmail.com>
2025-04-25 15:17:20 +0300

    add routes and authenticate_request function automatically

--- lib/generators/auth/jwt_generator.rb
+++ lib/generators/auth/jwt_generator.rb
@@ -1,5 +1,5 @@
 module Auth
-  class JwtGenerator < Rails::Generators::Base  # Changed from NamedBase
+  class JwtGenerator < Rails::Generators::Base
     desc "This generator creates JWT auth files (no arguments needed)"
     source_root File.expand_path('templates', __dir__)
 
@@ -8,6 +8,29 @@ module Auth
       template "authentication_controller.rb", "app/controllers/authentication_controller.rb"
       template "users_controller.rb", "app/controllers/users_controller.rb"
       template "user.rb", "app/models/user.rb"
+    end
+    
+    def modify_application_controller
+      inject_into_class "app/controllers/application_controller.rb", "ApplicationController" do
+        <<-RUBY
+  include JsonWebToken
+  before_action :authenticate_request
+  private
+  def authenticate_request
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
+  end
+        RUBY
+      end
+    end
+    
+    def add_routes
+      route <<-ROUTES
+  resources :users
+  post "/auth/login", to: "authentication#login"
+      ROUTES
     end
   end
 end
